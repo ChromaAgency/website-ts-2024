9ef919f6ae7a380ad9ae5dbb33d27534b9979bdc
Federico Gobea <fgobea@chroma.agency>
2025-01-30 19:47:42 -0500

    🐛

--- src/app/(pages)/(public)/(home)/Hero.tsx
+++ src/app/(pages)/(public)/(home)/Hero.tsx
@@ -1,48 +1,10 @@
-import Link from 'next/link'  
-import { FaArrowDownLong } from 'react-icons/fa6'  
-import Image from 'next/image'  
-import BackgroundImage from '@/assets/images/hero/Chroma_LogoWhiteBackground.png'  
+import Link from 'next/link'
+import { FaArrowDownLong } from 'react-icons/fa6'
+import Image from 'next/image'
+import ChromaLogo from '@/assets/images/hero/Chroma_LogoWhiteBackground.png'
 
-<<<<<<< HEAD
-const Hero = () => {  
-  return (  
-    <section className="py-44 relative bg-slate-500/5">  
-      <div className=" hero-with-shapes">  
-        <div className="shape1" />  
-        <div className="shape2" />  
-        <div className="shape3" />  
-        <div className="container">  
-          <div className="flex justify-between items-center">  
-            <div>  
-              <h1 className="md:text-7xl text-3xl text-[#310860] font-medium my-5">  
-                CHROMA&nbsp;  
-                <span className="relative after:bg-[#2ee0ca] md:after:h-6 after:h-4 after:w-full after:inset-x-0 after:bottom-0 after:absolute after:-z-10">  
-                  AGENCY  
-                </span>  
-              </h1>  
-              <h2 className="w-3/4 text-4xl font-medium mt-6 mb-20 text-[#8a50bc]">  
-                DIGITALIZACIÓN DE TU EMPRESA.  
-              </h2>  
-              
-              <div className="flex flex-wrap items-center gap-5">              
-                <Link  
-                  href="#portfolios"  
-                  className="flex gap-1 items-center py-3 px-6 rounded border border-[#310860] text-white bg-[#310860] hover:shadow-lg hover:text-[#310860] hover:bg-white hover:shadow-[#474747] focus:outline focus:outline-black/50 transition-all duration-500"  
-                >  
-                  <FaArrowDownLong /> Servicios  
-                </Link>  
-                {/*   
-                TODO: Add later  
-                <Link  
-                  href="contact-us"  
-                  className="py-3 px-6 rounded border border-black hover:border-black hover:bg-black hover:text-white hover:shadow-lg hover:shadow-black/50 focus:outline focus:outline-black/50 transition-all duration-500"  
-                >  
-                  Contactanos  
-                </Link> */}  
-              </div>  
-            </div>  
-=======
 const Hero = () => {
+
   return (
     <section className="py-44 relative bg-slate-500/5">
       <div className=" hero-with-shapes">
@@ -50,8 +12,9 @@ const Hero = () => {
         <div className="shape2" />
         <div className="shape3" />
         <div className="container">
-          <div>
-            {/* <div
+          <div className='flex align-top justify-start'>
+            <div>
+              {/* <div
               className="bg-amber-500/10 py-2 px-4 inline-block rounded-md mb-6"
               data-aos="fade-right"
               data-aos-duration={1000}
@@ -67,23 +30,23 @@ const Hero = () => {
                 </div>
               </Link>
             </div> */}
-            <h1 className="md:text-7xl text-3xl text-[#310860] font-medium my-5">
-              CHROMA&nbsp;
-              <span className="relative after:bg-[#2ee0ca] md:after:h-6 after:h-4 after:w-full after:inset-x-0 after:bottom-0 after:absolute after:-z-10">
-                AGENCY
-              </span>
-            </h1>
-            <h2 className="w-3/4 text-4xl font-medium mt-6 mb-20 text-[#8a50bc]">
-              DIGITALIZACIÓN DE TU EMPRESA.
-            </h2>
-            <div className="flex flex-wrap items-center gap-5">
-              <Link
-                href="#portfolios"
-                className="flex gap-1 items-center py-3 px-6 rounded border border-[#310860] text-white  bg-[#310860] hover:shadow-lg hover:text-[#310860] hover:bg-white  hover:shadow-[#474747] focus:outline focus:outline-black/50 transition-all duration-500"
-              >
-                <FaArrowDownLong /> Servicios
-              </Link>
-              {/* 
+              <h1 className="md:text-7xl text-3xl text-[#310860] font-medium my-5">
+                CHROMA&nbsp;
+                <span className="relative after:bg-[#2ee0ca] md:after:h-6 after:h-4 after:w-full after:inset-x-0 after:bottom-0 after:absolute after:-z-10">
+                  AGENCY
+                </span>
+              </h1>
+              <h2 className="w-3/4 text-4xl font-medium mt-6 mb-20 text-[#8a50bc]">
+                DIGITALIZACIÓN DE TU EMPRESA.
+              </h2>
+              <div className="flex flex-wrap items-center gap-5">
+                <Link
+                  href="#portfolios"
+                  className="flex gap-1 items-center py-3 px-6 rounded border border-[#310860] text-white  bg-[#310860] hover:shadow-lg hover:text-[#310860] hover:bg-white  hover:shadow-[#474747] focus:outline focus:outline-black/50 transition-all duration-500"
+                >
+                  <FaArrowDownLong /> Servicios
+                </Link>
+                {/* 
               TODO: Add later
               <Link
                 href="contact-us"
@@ -91,42 +54,21 @@ const Hero = () => {
               >
                 Contactanos
               </Link> */}
+
+              </div>
             </div>
+                <Image
+                  className="object-fit"
+                  src={ChromaLogo}
+                  width={600}
+                  height={600}
+                  alt="Chroma Agency Logo"
+                />
           </div>
         </div>
       </div>
-      {/* 
-      TODO: Add later
-      <div className="absolute end-0 top-48 hidden md:block">
-        <div className="flex items-center gap-5 vertical-rl px-2">
-          <Link href="" className="text-lg">
-            Twitter
-          </Link>
-          <Link href="" className="text-lg">
-            Facebook
-          </Link>
-          <Link href="" className="text-lg">
-            Instagram
-          </Link>
-        </div>
-      </div> */}
     </section>
   )
 }
->>>>>>> 9183b66851b4c1b4bab7672a74b5bd4ac94c1d2d
-
-            <Image  
-              className="max-w-full h-auto object-contain"  
-              src={BackgroundImage}  
-              width={800}  
-              height={800}  
-              alt="Chroma Agency Logo"  
-            />  
-          </div>  
-        </div>  
-      </div>  
-    </section>  
-  )  
-}  
 
 export default Hero
