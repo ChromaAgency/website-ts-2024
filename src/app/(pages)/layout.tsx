d8432abcc02a97f00ccf731ab171d20986f4c589
Federico Gobea <fgobea@chroma.agency>
2025-07-09 18:45:18 -0500

    fix(layout): agregar script de widget para mejorar la funcionalidad del layout

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic'
 import { ReactNode, Suspense } from 'react'
 import Footer from './(public)/(home)/Footer'
+import Script from 'next/script'
 const Navbar = dynamic(() => import('@/components/layout/Navbar'))
 const ScrollToTop = dynamic(() => import('@/components/ScrollToTop'))
 
@@ -19,9 +20,14 @@ const Layout = ({ children }: { children: ReactNode }) => {
         <ScrollToTop />
       </Suspense>
       <Footer />
-      </>
-
+      <Script
+        async
+        defer
+        src="https://agentes.numeral8.mx/js/widget/xic1logbwtuduptm/float.js"
+      />
+    </>
   )
 }
+
 
 export default Layout
