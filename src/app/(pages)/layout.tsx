8a0b748e5a92b5ed24294274f2b6e710f7aeb113
Federico Gobea <fgobea@chroma.agency>
2026-03-03 14:07:24 -0500

    Añadir Google Tag Manager al componente Layout

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -2,12 +2,24 @@ import dynamic from 'next/dynamic'
 import { ReactNode, Suspense } from 'react'
 import Footer from './(public)/(home)/Footer'
 import Script from 'next/script'
+import Head from 'next/head'
 const Navbar = dynamic(() => import('@/components/layout/Navbar'))
 const ScrollToTop = dynamic(() => import('@/components/ScrollToTop'))
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
+    <Head>
+      <Script id="google-tagmanager" strategy="afterInteractive">
+       {` <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5FH4GHJJ');</script>
+}<!-- End Google Tag Manager --> `}
+</Script>
+    </Head>
       <Suspense fallback={<div />}>
         <Navbar />
       </Suspense>
@@ -20,7 +32,12 @@ const Layout = ({ children }: { children: ReactNode }) => {
         <ScrollToTop />
       </Suspense>
       <Footer />
-
+      <Script id="google-tagmanager-noscript" strategy="afterInteractive">
+      {`<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FH4GHJJ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`}
+      </Script>
       {/* Google tag (gtag.js) */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-NZG45C6999"
